cc3ed8677f7f841fd39d78fab3fe7d8420109815
Guilherry Souza <guilherry.s@hotmail.com>
2026-01-28 22:12:08 -0300

    Create ProdutoService.java

---
+++ src/main/java/com/apiprodutos/api/service/ProdutoService.java
@@ -0,0 +1,32 @@
+package com.apiprodutos.api.service;
+
+import com.apiprodutos.api.model.Produto;
+import com.apiprodutos.api.repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ProdutoService {
+
+    private final ProdutoRepository repository;
+
+    public ProdutoService(ProdutoRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Produto> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Produto salvar(Produto produto) {
+        return repository.save(produto);
+    }
+
+    public Produto buscarPorId(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+}
